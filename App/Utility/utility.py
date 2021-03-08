be0584137b4377fb63a69a3b5cec8a48dcc4a3ad
steven <crestbootl@gmail.com>
2021-03-08 20:39:01 +0100

    Added utility

---
+++ App/Utility/utility.py
@@ -0,0 +1,12 @@
+import time
+from datetime import date
+
+
+def wait(hours):
+    time.sleep(hours)
+
+
+def write_log(*args):
+    with open(str(date.today()), "a") as cursor_file:
+        for a in args:
+            cursor_file.write(str(a) + "\n")
